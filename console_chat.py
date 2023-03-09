276dd45e6edcaf22a12e15e7592c6e92888bba7e
Milu <millby@yahoo.com>
2023-03-09 09:44:42 -0400

    feat(leaveConversation): add conversation log

--- console_chat.py
+++ console_chat.py
@@ -1,9 +1,15 @@
+import time
+
 import openai
 import os
 import json
 
 MODEL = "gpt-3.5-turbo"
-WELCOME_MSG = "[New Conversation] Using OpenAI Chat API("+MODEL+")."
+WELCOME_MSG = "[New Conversation] Using OpenAI Chat API(" + MODEL + ")."
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+CONVERSATION_LOG_PATH = CURRENT_DIR + '/conversation_log.json'
+CONFIG_PATH = CURRENT_DIR + '/config.json'
 
 
 def ask(key, msg):
@@ -20,12 +26,17 @@ def ask(key, msg):
     return result
 
 
+def save_conversation(c):
+    with open(CONVERSATION_LOG_PATH, encoding="utf-8", mode='a') as file:
+        c.insert(0, {"timestamp": int(time.time())})
+        file.write("\n," + json.dumps(c))
+
+
 if __name__ == '__main__':
     print(WELCOME_MSG)
     conversation = []
-    current_path = os.path.dirname(os.path.abspath(__file__))
 
-    with open(current_path+'/config.json', 'r') as f:
+    with open(CONFIG_PATH, 'r') as f:
         config = json.load(f)
         api_key = config['api_key']
 
@@ -34,11 +45,13 @@ if __name__ == '__main__':
         line = input()
         conversation.append({"role": "user", "content": line})
         if line == "clear":
+            save_conversation(conversation)
             conversation = []
             os.system('cls' if os.name == 'nt' else 'clear')
             print(WELCOME_MSG)
             continue
         elif line == "q":
+            save_conversation(conversation)
             break
         resp = ask(api_key, conversation)
         resp = resp.lstrip('\n')  # remove any \n before the actual reply
