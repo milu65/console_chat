7378f04d531c53b3d5c54c36372aa5280f752656
Boying Lei <by985314@dal.ca>
2023-03-14 10:51:47 -0300

    feat(dialogue): multi-line input is now supported fix(openaiResponse): add code to handle OpenAIError

--- console_chat.py
+++ console_chat.py
@@ -3,6 +3,7 @@ import time
 import openai
 import os
 import json
+from openai import OpenAIError
 
 MODEL = "gpt-3.5-turbo"
 WELCOME_MSG = "[New Conversation] Using OpenAI Chat API(" + MODEL + ")."
@@ -15,11 +16,16 @@ CONFIG_PATH = CURRENT_DIR + '/config.json'
 def ask(key, msg):
     openai.api_key = key
 
-    response = openai.ChatCompletion.create(
-        model=MODEL,
-        messages=msg,
-        # top_p=0.05
-    )
+    try:
+        response = openai.ChatCompletion.create(
+            model=MODEL,
+            messages=msg,
+            # top_p=0.05
+        )
+    except OpenAIError as e:
+        print(e)
+        return "null"
+
     result = ''
     for choice in response.choices:
         result += choice.message.content
@@ -41,10 +47,10 @@ if __name__ == '__main__':
         api_key = config['api_key']
 
     while True:
-        print(">", end="")
-        line = input()
-        conversation.append({"role": "user", "content": line})
-        if line == "clear":
+        line = input(">")
+        if line == "":
+            continue
+        elif line == "clear":
             save_conversation(conversation)
             conversation = []
             os.system('cls' if os.name == 'nt' else 'clear')
@@ -53,6 +59,17 @@ if __name__ == '__main__':
         elif line == "q":
             save_conversation(conversation)
             break
+        elif line == "m":
+            print("[Multi-line input enabled. A new line only including ~~~ for end of input]")
+            line = ""
+            while True:
+                i = input()
+                if "~~~" == i:
+                    print("[Multi-line input disabled]")
+                    break
+                line += i + "\n"
+        conversation.append({"role": "user", "content": line})
+
         resp = ask(api_key, conversation)
         resp = resp.lstrip('\n')  # remove any \n before the actual reply
         conversation.append({"role": "assistant", "content": resp})
